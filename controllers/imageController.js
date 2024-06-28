c3dfe684549adca217af6b1ab5232bfa99e1b1eb
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-06-28 10:00:21 +0300

    Refactor responses in imageController.js

--- controllers/imageController.js
+++ controllers/imageController.js
@@ -2,35 +2,26 @@ import Chat from "../models/chat.js";
 import User from "../models/user.js";
 
 import asyncHandler from "express-async-handler";
-import fs from "fs";
 
 const getChatImage = asyncHandler(async (req, res) => {
   const findChat = await Chat.findById(req.params.chatId);
-  let imageFile = "chat_placeholder.png";
-
-  if (fs.existsSync(`./assets/images/chats/${findChat.image}`)) {
-    imageFile = findChat.image;
-  }
+  let chatImage = findChat.image;
 
   if (findChat.participants.length === 2) {
     const findAnotherUser = await User.findById(findChat.participants[1]);
-    imageFile = findAnotherUser.profilePicture;
-    res.sendFile(imageFile, { root: "./assets/images/profiles" });
+    chatImage = findAnotherUser.profilePicture;
+    res.sendFile(chatImage, { root: "./assets/images/profiles" });
     return;
   }
 
-  res.sendFile(imageFile, { root: "./assets/images/chats" });
+  res.sendFile(chatImage, { root: "./assets/images/chats" });
 });
 
 const getContactImage = asyncHandler(async (req, res) => {
   const findContact = await User.findById(req.params.contactId);
-  let imageFile = "profile_placeholder.png";
-
-  if (fs.existsSync(`./assets/images/profiles/${findContact.profilePicture}`)) {
-    imageFile = findContact.profilePicture;
-  }
-
-  res.sendFile(imageFile, { root: "./assets/images/profiles" });
+  res.sendFile(findContact.profilePicture, {
+    root: "./assets/images/profiles",
+  });
 });
 
 export default {
