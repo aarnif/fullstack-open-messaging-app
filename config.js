3bc439a62353650459a4ff921b968a5a26594247
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-07-07 15:03:11 +0300

    Add configuration for cloudinary

--- config.js
+++ config.js
@@ -4,4 +4,11 @@ const MONGODB_USERNAME = process.env.MONGODB_USERNAME;
 const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
 const MONGODB_URI = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@cluster0.wr1sesb.mongodb.net/database?retryWrites=true&w=majority&appName=Cluster0`;
 
-export default { MONGODB_URI };
+const CLOUDINARY = {
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  secure: true,
+};
+
+export default { MONGODB_URI, CLOUDINARY };
