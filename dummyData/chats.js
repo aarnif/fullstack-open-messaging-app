e020ed586be80e0be285b410c692a9588c644df6
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-07-12 09:33:30 +0300

    Update dummyData/chats.js

--- dummyData/chats.js
+++ dummyData/chats.js
@@ -3,6 +3,7 @@ import messages from "./messages.js";
 const chats = [
   // Group chats
   {
+    _id: "6690cc6331f8d4e66b57ae22",
     title: "Weekend Hikers",
     description:
       "A group of enthusiasts who love to explore trails and mountains every weekend.",
@@ -12,6 +13,7 @@ const chats = [
     admin: null,
   },
   {
+    _id: "6690cc6331f8d4e66b57ae4c",
     title: "Book Club",
     description:
       "A club for avid readers to discuss and share their thoughts on various books.",
@@ -21,6 +23,7 @@ const chats = [
     admin: null,
   },
   {
+    _id: "6690cc6331f8d4e66b57aeb2",
     title: "Gaming Night",
     description:
       "A group dedicated to organizing and enjoying weekly gaming sessions.",
@@ -30,6 +33,7 @@ const chats = [
     admin: null,
   },
   {
+    _id: "6690cc6331f8d4e66b57af18",
     title: "Yoga Enthusiasts",
     description:
       "A community of yoga lovers who share tips, routines, and support each other.",
@@ -39,6 +43,7 @@ const chats = [
     admin: null,
   },
   {
+    _id: "6690cc6331f8d4e66b57afce",
     title: "Music Band",
     description:
       "A band of musicians collaborating on new music and sharing their passion.",
@@ -48,6 +53,7 @@ const chats = [
     admin: null,
   },
   {
+    _id: "6690cc6331f8d4e66b57b066",
     title: "Cooking Lovers",
     description:
       "A group for those who enjoy cooking and sharing recipes and culinary tips.",
@@ -57,6 +63,7 @@ const chats = [
     admin: null,
   },
   {
+    _id: "6690cc6331f8d4e66b57b0e0",
     title: "Photography Crew",
     description:
       "A collective of photographers who share their work and discuss techniques.",
@@ -66,6 +73,7 @@ const chats = [
     admin: null,
   },
   {
+    _id: "6690cc6331f8d4e66b57b19c",
     title: "Movie Buffs",
     description:
       "A group for movie enthusiasts to discuss films, actors, and upcoming releases.",
@@ -75,6 +83,7 @@ const chats = [
     admin: null,
   },
   {
+    _id: "6690cc6331f8d4e66b57b234",
     title: "Tech Geeks",
     description:
       "A community for tech enthusiasts to discuss the latest in technology and gadgets.",
@@ -84,6 +93,7 @@ const chats = [
     admin: null,
   },
   {
+    _id: "6690cc6331f8d4e66b57b29a",
     title: "Fitness Freaks",
     description:
       "A group of fitness enthusiasts sharing workouts, nutrition tips, and motivation.",
@@ -93,6 +103,7 @@ const chats = [
     admin: null,
   },
   {
+    _id: "6690cc6331f8d4e66b57b300",
     title: "Travel Buddies",
     description:
       "A group for travelers to share experiences, tips, and plan new adventures.",
