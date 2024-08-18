279ee3c90eef6d82a3930cf5c95cab93765d7ddf
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-08-18 15:51:24 +0300

    Remove local image assets and routing

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import config from "./config.js";
 import schema from "./graphql/schema.js";
 import User from "./models/user.js";
-import imageRouter from "./routes/imageRouter.js";
 
 import mongoose from "mongoose";
 import { ApolloServer } from "@apollo/server";
@@ -72,8 +71,6 @@ await server.start();
 
 app.use(cors());
 app.use(express.json());
-
-app.use("/images", imageRouter);
 
 app.use(
   "/",
--- routes/imageRouter.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import imageController from "../controllers/imageController.js";
-
-const route = express.Router();
-
-route.get("/chats/:chatId", imageController.getChatImage);
-route.get("/contacts/:contactId", imageController.getContactImage);
-
-export default route;
