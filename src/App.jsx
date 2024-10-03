b759d5847dfcf9825cafae985b13be0ef90d0742
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-10-03 14:38:52 +0300

    Add routing

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ const App = () => {
       <Route path="/signin" element={<div>Sign In</div>} />
       <Route path="/signup" element={<div>Sign Up</div>} />
       <Route path="/chats" element={<div>Chats</div>} />
-      <Route path="/chats/:id" element={<div>Chat with ID</div>} />
+      <Route path="/chats/:id" element={<div>Individual Chat</div>} />
       <Route path="/contacts" element={<div>Contacts</div>} />
-      <Route path="/contacts/:id" element={<div>Contact with ID</div>} />
+      <Route path="/contacts/:id" element={<div>Individual Contact</div>} />
       <Route path="/profile" element={<div>Profile</div>} />
       <Route path="/settings" element={<div>Settings</div>} />
       <Route path="*" element={<Navigate to="/" replace />} />
