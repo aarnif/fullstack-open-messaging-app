2e9be52e856ee23c2f14ae82427c05da9f103d96
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-10-03 14:37:55 +0300

    Add routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,18 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-      <h1 className="text-3xl font-bold text-red-600">
-        This is a Vite + React + Tailwind CSS app!
-      </h1>
-      <Routes>
-        <Route path="/" element={<Navigate to="/chats" replace />} />
-        <Route path="/signin" element={<div>Sign In</div>} />
-        <Route path="/signup" element={<div>Sign Up</div>} />
-        <Route path="/chats" element={<div>Chats</div>} />
-        <Route path="/chats/:id" element={<div>Chat with ID</div>} />
-        <Route path="/contacts" element={<div>Contacts</div>} />
-        <Route path="/contacts/:id" element={<div>Contact with ID</div>} />
-        <Route path="/profile" element={<div>Profile</div>} />
-        <Route path="/settings" element={<div>Settings</div>} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Navigate to="/chats" replace />} />
+      <Route path="/signin" element={<div>Sign In</div>} />
+      <Route path="/signup" element={<div>Sign Up</div>} />
+      <Route path="/chats" element={<div>Chats</div>} />
+      <Route path="/chats/:id" element={<div>Chat with ID</div>} />
+      <Route path="/contacts" element={<div>Contacts</div>} />
+      <Route path="/contacts/:id" element={<div>Contact with ID</div>} />
+      <Route path="/profile" element={<div>Profile</div>} />
+      <Route path="/settings" element={<div>Settings</div>} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 };
 
