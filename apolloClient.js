fd02dc6ad544ffb9f820faf496fd38a1c93911e5
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-10-04 09:39:00 +0300

    Add configuration for Apollo client

---
+++ apolloClient.js
@@ -0,0 +1,48 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+  split,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+import { getMainDefinition } from "@apollo/client/utilities";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("messagin-app-user-token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : null,
+    },
+  };
+});
+
+const httpLink = createHttpLink({ uri: import.meta.env.VITE_APOLLO_URI });
+
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: import.meta.env.VITE_APOLLO_WS_URI,
+  })
+);
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: splitLink,
+});
+
+export default client;
