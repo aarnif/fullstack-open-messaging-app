57009598dcb0ded0b631266bf6bf1f412fb1016d
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-10-05 14:16:30 +0300

    Add SignIn.jsx

---
+++ src/components/SignIn.jsx
@@ -0,0 +1,90 @@
+import useField from "../../hooks/useField";
+import { useNavigate } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+
+import { LOGIN } from "../../graphql/mutations";
+import useNotifyMessage from "../../hooks/useNotifyMessage";
+import Notify from "./Notify";
+
+const SignIn = () => {
+  const navigate = useNavigate();
+  const username = useField("text", "Enter your username here...");
+  const password = useField("password", "Enter your password here...");
+
+  const showNotifyMessage = useNotifyMessage();
+
+  const [mutate] = useMutation(LOGIN, {
+    onError: (error) => {
+      console.log(error.graphQLErrors[0].message);
+      showNotifyMessage.show(error.graphQLErrors[0].message);
+    },
+  });
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    console.log("Logging in...");
+
+    const { data } = await mutate({
+      variables: { username: username.value, password: password.value },
+    });
+
+    console.log(data);
+
+    if (data) {
+      console.log("Logged in successfully!");
+      localStorage.setItem("messaging-app-user-token", data.login.value);
+      navigate("/chats");
+    }
+  };
+
+  const handleClickSignUp = () => {
+    navigate("/signup");
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h1 data-testid="sign-in-header">Sign In</h1>
+        <ul>
+          <Notify notifyMessage={showNotifyMessage} />
+          <li>
+            <label>USERNAME:</label>
+          </li>
+          <li>
+            <input {...username} />
+          </li>
+
+          {username.value.length === 0 && (
+            <li>
+              <span>Please enter your username</span>
+            </li>
+          )}
+
+          <li>
+            <label>PASSWORD:</label>
+          </li>
+          <li>
+            <input {...password} />
+          </li>
+          {password.value.length === 0 && (
+            <li>
+              <span>Please enter your password</span>
+            </li>
+          )}
+          <li>
+            <button type="submit">Sign In</button>
+          </li>
+          <li>
+            <span>Don't have an account?</span>
+            <button onClick={handleClickSignUp} type="button">
+              Sign Up Here
+            </button>
+          </li>
+        </ul>
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
