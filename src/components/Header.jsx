f987aef441ad9f0e2382a79da41c20d3cff25d18
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-10-05 16:54:56 +0300

    Add Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <div className="w-full flex flex-row justify-center items-center py-2 bg-green-600 shadow-lg">
+      <div className="flex flex-row w-full max-w-4xl">
+        <div className="flex-grow flex justify-center items-center">
+          <h1 className="text-xl text-white font-bold">Messaging App</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
