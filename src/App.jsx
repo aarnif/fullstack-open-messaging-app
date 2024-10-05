bbe11b9e69532b13a9be170f3668ee1c6e03fbd4
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-10-05 17:11:44 +0300

    Add Header and Footer to App

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import { useQuery, useApolloClient } from "@apollo/client";
 
 import { GET_CURRENT_USER } from "../graphql/queries";
+import Header from "./components/Header";
 import ProtectedRoutes from "./components/ProtectedRoutes";
 import SignIn from "./components/SignIn";
+import Footer from "./components/Footer";
 
 const App = () => {
   const client = useApolloClient();
@@ -23,24 +25,31 @@ const App = () => {
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Navigate to="/chats" replace />} />
-        <Route
-          path="/signin"
-          element={data?.me ? <Navigate to="/chats" replace /> : <SignIn />}
-        />
-        <Route path="/signup" element={<div>Sign Up</div>} />
-        <Route element={<ProtectedRoutes user={data?.me} />}>
-          <Route path="/chats" element={<div>Chats</div>} />
-          <Route path="/chats/:id" element={<div>Individual Chat</div>} />
-          <Route path="/contacts" element={<div>Contacts</div>} />
-          <Route path="/contacts/:id" element={<div>Individual Contact</div>} />
-          <Route path="/profile" element={<div>Profile</div>} />
-          <Route path="/settings" element={<div>Settings</div>} />
-        </Route>
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-      <button onClick={handleLogout}>Logout</button>
+      <Header user={data?.me} />
+      <main className="flex-grow flex">
+        <Routes>
+          <Route path="/" element={<Navigate to="/chats" replace />} />
+          <Route
+            path="/signin"
+            element={data?.me ? <Navigate to="/chats" replace /> : <SignIn />}
+          />
+          <Route path="/signup" element={<div>Sign Up</div>} />
+          <Route element={<ProtectedRoutes user={data?.me} />}>
+            <Route path="/chats" element={<div>Chats</div>} />
+            <Route path="/chats/:id" element={<div>Individual Chat</div>} />
+            <Route path="/contacts" element={<div>Contacts</div>} />
+            <Route
+              path="/contacts/:id"
+              element={<div>Individual Contact</div>}
+            />
+            <Route path="/profile" element={<div>Profile</div>} />
+            <Route path="/settings" element={<div>Settings</div>} />
+          </Route>
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </main>
+      {data?.me && <button onClick={handleLogout}>Logout</button>}
+      <Footer />
     </>
   );
 };
