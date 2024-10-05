9d0cbb2fe46e998704dbe6a9585ac5bc4794e773
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-10-05 16:57:42 +0300

    Update Notify.jsx

--- src/components/Notify.jsx
+++ src/components/Notify.jsx
@@ -2,7 +2,11 @@ const Notify = ({ notifyMessage }) => {
   if (!notifyMessage || !notifyMessage.content) {
     return null;
   }
-  return <div className="text-red-500">{notifyMessage.content}</div>;
+  return (
+    <div className="p-2 flex justify-center items-center bg-red-400 rounded-lg">
+      <span className="text-md text-red-700">{notifyMessage.content}</span>
+    </div>
+  );
 };
 
 export default Notify;
