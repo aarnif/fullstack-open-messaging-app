28fff2d28b0274567eaf3b3dea75ac2e49ab61a9
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-10-05 17:41:48 +0300

    Update createUser-mutation

--- graphql/mutations.js
+++ graphql/mutations.js
@@ -10,8 +10,16 @@ export const LOGIN = gql`
 `;
 
 export const CREATE_USER = gql`
-  mutation CreateUser($username: String!, $password: String!) {
-    createUser(username: $username, password: $password) {
+  mutation CreateUser(
+    $username: String!
+    $password: String!
+    $confirmPassword: String!
+  ) {
+    createUser(
+      username: $username
+      password: $password
+      confirmPassword: $confirmPassword
+    ) {
       username
     }
   }
