6374b71928ff59250c379ef50e4587e3d233ff8e
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-10-06 15:28:33 +0300

    Add ChatItem.jsx

---
+++ src/components/Chats/ChatItem.jsx
@@ -0,0 +1,46 @@
+import { useNavigate } from "react-router-dom";
+import ChatCard from "./ChatCard";
+
+const ChatItem = ({ user, item, activePath, setActivePath }) => {
+  const navigate = useNavigate();
+
+  const handlePress = () => {
+    console.log("Pressed chat titled:", item.title);
+    navigate(`/chats/${item.id}`);
+    setActivePath(item.id);
+  };
+
+  if (!item.messages.length) {
+    return (
+      <div
+        className="flex items-center my-2 mx-4 cursor-pointer"
+        onClick={handlePress}
+      >
+        <div className="mr-4">
+          <img
+            src={item.displayChatImage.thumbnail}
+            alt="Chat Thumbnail"
+            style={{ width: 48, height: 48, borderRadius: "9999px" }}
+          />
+        </div>
+        <div className="flex-1">
+          <div className="text-md font-bold">{item.displayChatTitle}</div>
+          <div className="text-slate-700">No messages</div>
+        </div>
+      </div>
+    );
+  }
+
+  const classStyles =
+    activePath === item.id
+      ? "w-full flex items-start py-2 px-4 border-b bg-gray-200 hover:bg-gray-200 transition"
+      : "w-full flex items-start py-2 px-4 border-b hover:bg-gray-200 transition";
+
+  return (
+    <button className={classStyles} onClick={handlePress}>
+      <ChatCard user={user} chat={item} />
+    </button>
+  );
+};
+
+export default ChatItem;
