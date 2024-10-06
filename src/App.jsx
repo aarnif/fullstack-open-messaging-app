65bbea76e5a48296f7c50135fe7be61d37f07d59
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-10-06 11:37:55 +0300

    Add Menu to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
-import { useQuery, useApolloClient } from "@apollo/client";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useQuery } from "@apollo/client";
 
 import { GET_CURRENT_USER } from "../graphql/queries";
 import Header from "./components/Header";
@@ -7,34 +7,31 @@ import ProtectedRoutes from "./components/ProtectedRoutes";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 import Footer from "./components/Footer";
+import Menu from "./components/Menu";
 
 const App = () => {
-  const client = useApolloClient();
   const { data, error, loading } = useQuery(GET_CURRENT_USER);
-  const navigate = useNavigate();
   console.log("Current user:", data);
 
   if (loading) {
     return <div>Loading...</div>;
   }
 
-  const handleLogout = () => {
-    localStorage.removeItem("messaging-app-user-token");
-    client.resetStore();
-    navigate("/signin");
-  };
-
   return (
     <>
       <Header user={data?.me} />
       <main className="flex-grow flex">
+        {data?.me && <Menu />}
         <Routes>
           <Route path="/" element={<Navigate to="/chats" replace />} />
           <Route
             path="/signin"
             element={data?.me ? <Navigate to="/chats" replace /> : <SignIn />}
           />
-          <Route path="/signup" element={<SignUp />} />
+          <Route
+            path="/signup"
+            element={data?.me ? <Navigate to="/chats" replace /> : <SignUp />}
+          />
           <Route element={<ProtectedRoutes user={data?.me} />}>
             <Route path="/chats" element={<div>Chats</div>} />
             <Route path="/chats/:id" element={<div>Individual Chat</div>} />
@@ -49,7 +46,6 @@ const App = () => {
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </main>
-      {data?.me && <button onClick={handleLogout}>Logout</button>}
       <Footer />
     </>
   );
