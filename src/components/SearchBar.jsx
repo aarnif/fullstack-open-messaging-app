52ba785dd95477eb2c06b80e1a32fbb8c47de8a7
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-10-11 16:41:59 +0300

    Update SearchBar-styles

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,10 +2,10 @@ import { FaSearch } from "react-icons/fa";
 
 const SearchBar = ({ searchWord }) => {
   return (
-    <div className="flex justify-center items-center my-2 p-2 border border-slate-700 rounded-lg bg-white hover:border-2 hover:border-green-600 focus-within:border-green-600 transition">
+    <div className="flex justify-center items-center my-2 p-2 border-2 border-white rounded-lg bg-slate-100 hover:border-green-600 focus-within:border-green-600 transition">
       <FaSearch className="w-4 h-4 text-slate-700 fill-current" />
       <input
-        className="w-full pl-2 text-slate-700 focus:outline-none focus:bg-opacity-0"
+        className="w-full pl-2 text-slate-700 bg-slate-100 focus:outline-none focus:bg-opacity-0"
         {...searchWord}
       />
     </div>
