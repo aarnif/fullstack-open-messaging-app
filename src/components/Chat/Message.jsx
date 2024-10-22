1ba9426537f590a0323c2b4309ea018dc5237257
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-10-22 17:52:06 +0300

    Add speech bubble tails

--- src/components/Chat/Message.jsx
+++ src/components/Chat/Message.jsx
@@ -13,7 +13,7 @@ const NotificationMessage = ({ message }) => {
 const MessageByAnotherUser = ({ message, handlePressImage }) => {
   return (
     <div className="mr-2 my-2 flex flex-col items-start">
-      <div className="min-w-[100px] max-w-[600px] ml-8 pt-2 px-2 flex flex-col bg-slate-300 rounded-lg">
+      <div className="min-w-[100px] max-w-[600px] ml-8 pt-2 px-2 flex flex-col bg-slate-300 rounded-lg relative">
         <div className="text-slate-800 font-bold">{message.sender.name}</div>
         {message.image.thumbnail && (
           <div>
@@ -41,11 +41,12 @@ const MessageByAnotherUser = ({ message, handlePressImage }) => {
         <div className="my-1 text-end text-xs text-slate-800 ">
           {helpers.formatMessageTime(message.createdAt)}
         </div>
+        <div className="absolute bottom-0 -left-2 border-t-[16px] border-t-transparent border-r-[16px] border-r-slate-300"></div>
       </div>
       <img
         src={message.sender.image.thumbnail}
         alt="sender-thumbnail"
-        className="w-12 h-12 rounded-full"
+        className="relative right-[12px] w-12 h-12 rounded-full"
       />
     </div>
   );
@@ -54,7 +55,7 @@ const MessageByAnotherUser = ({ message, handlePressImage }) => {
 const MessageByCurrentUser = ({ message, handlePressImage }) => {
   return (
     <div className="mr-2 my-2 flex flex-col items-end">
-      <div className="min-w-[100px] max-w-[600px] mr-8 pt-2 px-2 flex flex-col bg-green-300 rounded-lg">
+      <div className="min-w-[100px] max-w-[600px] mr-8 pt-2 px-2 flex flex-col bg-green-300 rounded-lg relative">
         <div className="text-slate-800 font-bold">You</div>
         {message.image.thumbnail && (
           <div>
@@ -82,6 +83,7 @@ const MessageByCurrentUser = ({ message, handlePressImage }) => {
         <div className="my-1 text-xs text-slate-800 text-end">
           {helpers.formatMessageTime(message.createdAt)}
         </div>
+        <div className="absolute bottom-0 -right-2 border-t-[16px] border-t-transparent border-l-[16px] border-l-green-300"></div>
       </div>
     </div>
   );
