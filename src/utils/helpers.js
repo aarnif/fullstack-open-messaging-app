261bdafa8c2049a410f95dd5c23a0b5b54b81e3b
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-10-23 18:06:03 +0300

    Add capitalizeString to helpers.js

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -40,9 +40,14 @@ const sortChatsByDate = (chats) =>
     );
   });
 
+const capitalizeString = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
 export default {
   formatMessageTime,
   sliceLatestMessage,
   newMessagesCount,
   sortChatsByDate,
+  capitalizeString,
 };
