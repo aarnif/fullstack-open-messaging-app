6ff6a4688b988e05e1dd28d431935acf6afd5a91
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-10-24 10:52:47 +0300

    Update SearchBar- and Menu-styles

--- src/components/MenuHeader.jsx
+++ src/components/MenuHeader.jsx
@@ -5,9 +5,11 @@ const MenuHeader = ({ title, handleCallBack, searchWord }) => {
   return (
     <div className="p-4">
       <div className="flex justify-between items-center">
-        <h1 className="text-2xl text-slate-800 font-bold">{title}</h1>
+        <h1 className="text-2xl text-slate-800 dark:text-slate-100 font-bold">
+          {title}
+        </h1>
         <button onClick={handleCallBack}>
-          <MdOpenInNew className="w-7 h-7 text-slate-700 fill-current" />
+          <MdOpenInNew className="w-7 h-7 text-slate-700 dark:text-slate-100 fill-current" />
         </button>
       </div>
 
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -2,10 +2,10 @@ import { FaSearch } from "react-icons/fa";
 
 const SearchBar = ({ searchWord }) => {
   return (
-    <div className="flex justify-center items-center my-2 p-2 border-2 border-white rounded-lg bg-slate-100 hover:border-green-600 focus-within:border-green-600 transition">
-      <FaSearch className="w-4 h-4 text-slate-700 fill-current" />
+    <div className="flex justify-center items-center my-2 p-2 border-2 border-slate-100 dark:border-slate-500 rounded-lg bg-slate-100 dark:bg-slate-500 hover:border-violet-500 focus-within:border-violet-500 transition">
+      <FaSearch className="w-4 h-4 text-slate-800 dark:text-slate-100 fill-current" />
       <input
-        className="w-full pl-2 text-slate-700 bg-slate-100 focus:outline-none focus:bg-opacity-0"
+        className="w-full pl-2 text-slate-800 dark:text-slate-100 placeholder:text-slate-800 dark:placeholder:text-slate-100 bg-slate-100 dark:bg-slate-500 focus:outline-none focus:bg-opacity-0"
         {...searchWord}
       />
     </div>
