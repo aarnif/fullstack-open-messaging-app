5e7af0782efd71204aa7a5ee6ae324ebc24754a9
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-10-28 09:17:09 +0200

    Update group chat info page styles

--- src/components/Modals/GroupChatInfoModal/ChatMembersList.jsx
+++ src/components/Modals/GroupChatInfoModal/ChatMembersList.jsx
@@ -2,9 +2,9 @@ import ChatMember from "./ChatMember";
 
 const ChatMembersList = ({ user, chatParticipants, admin }) => {
   return (
-    <div className="mt-8 flex-grow w-full max-w-[600px] bg-white">
-      <div className="w-full py-2 flex flex-col justify-center items-start bg-white">
-        <div className="mx-4 text-xl text-slate-800 font-bold">
+    <div className="mt-8 flex-grow w-full max-w-[600px]">
+      <div className="w-full py-2 flex flex-col justify-center items-start">
+        <div className="mx-4 text-xl text-slate-800 dark:text-slate-100 font-bold">
           {`${chatParticipants.length} members`}
         </div>
       </div>
--- src/components/Modals/GroupChatInfoModal/GroupChatInfoModal.jsx
+++ src/components/Modals/GroupChatInfoModal/GroupChatInfoModal.jsx
@@ -43,36 +43,36 @@ const GroupChatInfoModal = ({ user, chat, setShowChatInfoModal }) => {
   };
 
   return (
-    <div className="z-10 absolute top-0 left-0 w-full h-full flex flex-col bg-white">
+    <div className="z-10 absolute top-0 left-0 w-full h-full flex flex-col bg-slate-50 dark:bg-slate-700">
       <div className="m-4 flex justify-between">
         <button onClick={() => setShowChatInfoModal(false)}>
-          <IoChevronBack className="w-7 h-7 text-slate-700 fill-current" />
+          <IoChevronBack className="w-7 h-7 text-slate-700 dark:text-slate-100 fill-current" />
         </button>
         <button onClick={handleEditChat}>
-          <FiEdit className="w-7 h-7" />
+          <FiEdit className="w-7 h-7 text-slate-700 dark:text-slate-100" />
         </button>
       </div>
-      <div className="w-full py-4 flex flex-col justify-center items-center bg-white">
+      <div className="w-full py-4 flex flex-col justify-center items-center">
         <ClickableImage
           imageUri={chat.image.thumbnail}
           imageAlt={`${chat.title} image`}
           fullScreenImageUri={chat.image.original}
         />
-        <div className="pt-4 text-xl text-slate-800 font-bold">
+        <div className="pt-4 text-xl text-slate-800 dark:text-slate-100 font-bold">
           {chat.title}
         </div>
-        <div className="mx-8 text-sm text-slate-800 text-center">
+        <div className="mx-8 text-sm text-slate-700 dark:text-slate-200 text-center">
           {!chat.description.length ? "No description" : chat.description}
         </div>
       </div>
-      <div className="w-full flex-grow flex flex-col justify-center items-center bg-white">
+      <div className="w-full flex-grow flex flex-col justify-center items-center">
         <ChatMembersList
           user={user}
           chatParticipants={chat.participants}
           admin={chatAdmin}
         />
         {user.id !== chatAdmin.id && (
-          <div className="w-full p-4 flex flex-col justify-center items-start bg-white">
+          <div className="w-full p-4 flex flex-col justify-center items-start">
             <button
               onClick={handleLeaveChat}
               className="mb-2 w-full flex-grow max-h-[60px] p-2 flex justify-center items-center border-2 border-slate-200 bg-slate-200 rounded-xl"
