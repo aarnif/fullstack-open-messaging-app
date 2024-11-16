3cf93c20a9aa4554ef75bad5f5390ed4f2644e4f
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-11-16 18:37:09 +0200

    Add vite-envs to config.js

--- backend/config.js
+++ backend/config.js
@@ -7,4 +7,15 @@ const MONGODB_URI =
     ? process.env.TEST_MONGODB_URI
     : process.env.MONGODB_URI;
 
-export default { PORT, JWT_SECRET, MONGODB_URI };
+const VITE_APOLLO_URI = process.env.VITE_APOLLO_URI;
+const VITE_APOLLO_WS_URI = process.env.VITE_APOLLO_WS_URI;
+const VITE_IMGBB_API_KEY = process.env.VITE_IMGBB_API_KEY;
+
+export default {
+  PORT,
+  JWT_SECRET,
+  MONGODB_URI,
+  VITE_APOLLO_URI,
+  VITE_APOLLO_WS_URI,
+  VITE_IMGBB_API_KEY,
+};
