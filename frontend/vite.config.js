f70b7a6c56636c824622a0b53b4bf5cdbef98e3f
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-11-16 18:37:43 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import config from "../backend/config.js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "process.env": {
+      VITE_APOLLO_URI: config.VITE_APOLLO_URI,
+      VITE_APOLLO_WS_URI: config.VITE_APOLLO_WS_URI,
+      VITE_IMGBB_API_KEY: config.VITE_IMGBB_API_KEY,
+    },
+  },
+  base: "/",
+  root: "frontend",
   test: {
     environment: "jsdom",
     globals: true,
