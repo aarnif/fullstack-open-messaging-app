b28ad0b16681365737dd656e7650b846436b64cc
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-11-17 16:13:14 +0200

    Add tests.spec.js

---
+++ e2e-tests/tests.spec.js
@@ -0,0 +1,12 @@
+// @ts-check
+import { test, expect } from "@playwright/test";
+
+test.describe("Messaging app", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("http://localhost:5173");
+  });
+
+  test("has title", async ({ page }) => {
+    await expect(page).toHaveTitle(/Messaging App/);
+  });
+});
