3f775c8b18a3bcddc5beae290eb0ac7311f53b91
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-11-20 19:25:35 +0200

    Implement edit profile

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -1,12 +1,32 @@
+import { useState } from "react";
+import { AnimatePresence } from "framer-motion";
+import { FiEdit } from "react-icons/fi";
+
 import IndividualContactCard from "../IndividualContactCard/IndividualContactCard";
+import EditProfileModal from "../Modals/EditProfileModal/EditProfileModal";
 
 const Profile = ({ user, menuComponent }) => {
+  const [showEditProfileModal, setShowEditProfileModal] = useState(false);
+
   return (
     <div className="flex-grow flex bg-slate-50 dark:bg-slate-700">
       {menuComponent}
-      <div className="flex-grow flex justify-center items-start">
+      <div className="relative flex-grow flex justify-center items-start">
         <div className="flex-grow p-8 flex flex-col justify-start items-center">
+          <div className="m-4 w-full flex justify-end">
+            <button onClick={() => setShowEditProfileModal(true)}>
+              <FiEdit className="w-7 h-7 text-slate-700 dark:text-slate-100" />
+            </button>
+          </div>
           <IndividualContactCard user={user} contact={user} />
+          <AnimatePresence>
+            {showEditProfileModal && (
+              <EditProfileModal
+                user={user}
+                setShowEditProfileModal={setShowEditProfileModal}
+              />
+            )}
+          </AnimatePresence>
         </div>
       </div>
     </div>
