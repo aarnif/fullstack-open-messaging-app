7ec5b235fc82db68f4a7e796b09723544a24d2ea
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2024-12-28 09:39:59 +0200

    Update config.js

--- config.js
+++ config.js
@@ -10,16 +10,19 @@ const MONGODB_URI =
 const VITE_APOLLO_URI =
   process.env.NODE_ENV === "test" || process.env.NODE_ENV === "development"
     ? `http://localhost:${PORT}`
-    : "https://fullstack-open-messaging-app-api.onrender.com/";
+    : process.env.VITE_APOLLO_URI;
 
 const VITE_APOLLO_WS_URI =
   process.env.NODE_ENV === "test" || process.env.NODE_ENV === "development"
     ? `ws://localhost:${PORT}`
-    : "wss://fullstack-open-messaging-app-api.onrender.com/";
+    : process.env.VITE_APOLLO_WS_URI;
 
 const VITE_IMGBB_API_KEY = process.env.VITE_IMGBB_API_KEY;
 
-const REDIS_URI = process.env.REDIS_URI;
+const REDIS_URI =
+  process.env.NODE_ENV === "test" || process.env.NODE_ENV === "development"
+    ? process.env.TEST_REDIS_URI
+    : process.env.REDIS_URI;
 
 export default {
   PORT,
