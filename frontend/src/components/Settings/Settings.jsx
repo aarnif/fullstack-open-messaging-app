31f21544624f934310708552468c230c75e70006
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-01-18 21:54:48 +0200

    Make settings page responsive

--- frontend/src/components/Settings/Settings.jsx
+++ frontend/src/components/Settings/Settings.jsx
@@ -3,7 +3,9 @@ import SettingsCard from "./SettingsCard";
 const Settings = ({ user, menuComponent }) => {
   return (
     <div className="flex-grow flex bg-slate-50 dark:bg-slate-700">
-      {menuComponent}
+      <div className="hidden flex-grow lg:max-w-[450px] lg:flex">
+        {menuComponent}
+      </div>
       <div className="flex-grow flex justify-center items-start">
         <div className="flex-grow max-w-[1000px] p-8 flex flex-col justify-start items-center">
           <SettingsCard user={user} />
