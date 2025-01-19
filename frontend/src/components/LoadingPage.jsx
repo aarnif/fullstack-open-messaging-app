7c01d8c4f2f5f7363462b23238e1ffbdf5b1191d
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-01-19 16:33:11 +0200

    Tweak styles in LoadingPage.jsx

--- frontend/src/components/LoadingPage.jsx
+++ frontend/src/components/LoadingPage.jsx
@@ -9,7 +9,7 @@ const LoadingPage = () => {
       exit={{ opacity: 0, transition: { duration: 0.5 } }}
     >
       <Loading loadingText="Loading page..." />
-      <h1 className="fixed bottom-16 text-3xl text-slate-800 dark:text-slate-100 font-bold">
+      <h1 className="fixed bottom-16 text-2xl sm:text-3xl text-slate-800 dark:text-slate-100 font-bold">
         Messaging App
       </h1>
     </motion.div>
