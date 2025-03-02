554495a5f51ae0a95021ee2c8958a5990c826bf8
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-03-02 15:26:27 +0200

    Remove footer and add background image

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,15 @@
+import { useLocation } from "react-router";
+
 const Header = () => {
+  const location = useLocation();
+  const condition =
+    location.pathname !== "/signin" && location.pathname !== "/signup";
   return (
-    <div className="w-full flex flex-row justify-center items-center py-2 bg-white shadow-lg dark:bg-slate-900">
+    <div
+      className={`w-full flex flex-row justify-center items-center py-2 ${
+        condition && "bg-white shadow-lg dark:bg-slate-900"
+      }`}
+    >
       <div className="flex flex-row w-full max-w-4xl">
         <div className="flex-grow flex justify-center items-center">
           <h1 className="text-xl text-slate-800 dark:text-slate-100 font-bold">
