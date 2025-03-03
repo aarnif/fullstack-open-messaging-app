986cda1db03d22ef2f01a3b4ef45bd9324094c3b
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-03-03 18:12:31 +0200

    Refactor confirm modals in ModalProvider.jsx

--- frontend/src/components/ModalProvider.jsx
+++ frontend/src/components/ModalProvider.jsx
@@ -40,7 +40,8 @@ const AlertModal = ({ title, message, closeModal }) => {
   );
 };
 
-const SuccessModal = ({
+const ConfirmModal = ({
+  variant,
   title,
   message,
   confirmText,
@@ -53,20 +54,46 @@ const SuccessModal = ({
       animate={{ y: 0, opacity: 1, duration: 0.4 }}
       exit={{ y: 50, opacity: 0 }}
       transition={{ delay: 0.4, type: "tween" }}
-      className="bg-green-50 rounded-lg border-l-4 border-green-900"
+      className={`rounded-lg border-l-4 ${
+        variant === "success"
+          ? "border-green-900 bg-green-50"
+          : "border-red-900 bg-red-50"
+      }`}
     >
       <div className="w-full px-12 py-8 md:px-14 md:py-10 flex flex-col gap-4">
         <div className="flex justify-start items-center gap-3">
-          <div className="w-8.5 h-8.5 md:w-10 md:h-10 flex justify-center items-center rounded-full bg-green-300">
-            <div className="w-6 h-6 md:w-7 md:h-7 flex justify-center items-center rounded-full bg-green-100 border-2 border-green-400">
-              <FaExclamation className="w-4 h-4 md:w-4.5 md:h-4.5 text-green-900 fill-current" />
+          <div
+            className={`w-8.5 h-8.5 md:w-10 md:h-10 flex justify-center items-center rounded-full ${
+              variant === "success" ? "bg-green-300" : "bg-red-300"
+            }`}
+          >
+            <div
+              className={`w-6 h-6 md:w-7 md:h-7 flex justify-center items-center rounded-full border-2 ${
+                variant === "success"
+                  ? "border-green-400 bg-green-100"
+                  : "border-red-400 bg-red-100"
+              }`}
+            >
+              <FaExclamation
+                className={`w-4 h-4 md:w-4.5 md:h-4.5 fill-current ${
+                  variant === "success" ? "text-green-900" : "text-red-900"
+                }`}
+              />
             </div>
           </div>
-          <h2 className="text-center text-green-900 text-lg md:text-xl font-semibold font-robo-condensed">
+          <h2
+            className={`text-center ${
+              variant === "success" ? "text-green-900" : "text-red-900"
+            } text-lg md:text-xl font-semibold font-robo-condensed`}
+          >
             {title}
           </h2>
         </div>
-        <div className="self-stretch h-12 text-green-700 text-sm md:text-base">
+        <div
+          className={`w-full h-12 ${
+            variant === "success" ? "text-green-700" : "text-red-700"
+          } text-sm md:text-base`}
+        >
           {message}
         </div>
         <div className="flex justify-center space-x-2">
@@ -79,59 +106,11 @@ const SuccessModal = ({
           </button>
           <button
             data-testid="confirm-button"
-            className="w-full px-4 py-2 bg-green-200 rounded-lg border border-green-200 shadow-xl flex justify-center items-center text-green-900 cursor-pointer text-sm md:text-base font-bold
-              hover:bg-green-300 active:bg-green-300 active:border-green-400 active:inset-shadow-sm transition-all duration-300 ease-in-out"
-            onClick={onConfirm}
-          >
-            {confirmText}
-          </button>
-        </div>
-      </div>
-    </motion.div>
-  );
-};
-
-const DangerModal = ({
-  title,
-  message,
-  confirmText,
-  closeModal,
-  onConfirm,
-}) => {
-  return (
-    <motion.div
-      initial={{ y: 50, opacity: 0 }}
-      animate={{ y: 0, opacity: 1, duration: 0.4 }}
-      exit={{ y: 50, opacity: 0 }}
-      transition={{ delay: 0.4, type: "tween" }}
-      className="bg-red-50 rounded-lg border-l-4 border-red-900"
-    >
-      <div className="w-full px-12 py-8 md:px-14 md:py-10 flex flex-col gap-4">
-        <div className="flex justify-start items-center gap-3">
-          <div className="w-8.5 h-8.5 md:w-10 md:h-10 flex justify-center items-center rounded-full bg-red-300">
-            <div className="w-6 h-6 md:w-7 md:h-7 flex justify-center items-center rounded-full bg-red-100 border-2 border-red-400">
-              <FaExclamation className="w-4 h-4 md:w-4.5 md:h-4.5 text-red-900 fill-current" />
-            </div>
-          </div>
-          <h2 className="text-center text-red-900 text-lg md:text-xl font-semibold font-robo-condensed">
-            {title}
-          </h2>
-        </div>
-        <div className="self-stretch h-12 text-red-700 text-sm md:text-base">
-          {message}
-        </div>
-        <div className="flex justify-center space-x-2">
-          <button
-            onClick={closeModal}
-            data-testid="cancel-button"
-            className="w-full flex justify-center items-center text-base font-medium text-slate-700 dark:text-slate-200 hover:underline transition"
-          >
-            Cancel
-          </button>
-          <button
-            data-testid="confirm-button"
-            className="w-full px-4 py-2  bg-red-200 rounded-lg border border-red-200 shadow-xl flex justify-center items-center text-red-900 cursor-pointer text-sm md:text-base font-bold
-              hover:bg-red-300 active:bg-red-300 active:border-red-400 active:inset-shadow-sm transition-all duration-300 ease-in-out"
+            className={`w-full px-4 py-2 rounded-lg border shadow-xl flex justify-center items-center cursor-pointer text-sm md:text-base font-bold transition-all duration-300 ease-in-out ${
+              variant === "success"
+                ? "bg-green-200 border-green-200 text-green-900 hover:bg-green-300 active:bg-green-300 active:border-green-400 active:inset-shadow-sm"
+                : "bg-red-200 border-red-200 text-red-900 hover:bg-red-300 active:bg-red-300 active:border-red-400 active:inset-shadow-sm"
+            }`}
             onClick={onConfirm}
           >
             {confirmText}
@@ -180,7 +159,8 @@ const ModalProvider = ({ children }) => {
       <AlertModal title={title} message={message} closeModal={closeModal} />
     ),
     success: (
-      <SuccessModal
+      <ConfirmModal
+        variant="success"
         title={title}
         message={message}
         confirmText={confirmText}
@@ -189,7 +169,8 @@ const ModalProvider = ({ children }) => {
       />
     ),
     danger: (
-      <DangerModal
+      <ConfirmModal
+        variant="danger"
         title={title}
         message={message}
         confirmText={confirmText}
