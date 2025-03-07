ad044240b63aae3544c50202bd7c496b74dd489d
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-03-07 19:26:15 +0200

    Add new tests to server.test.js

--- backend/tests/server.test.js
+++ backend/tests/server.test.js
@@ -18,14 +18,19 @@ const credentials = {
 
 const contactDetails = [
   {
-    id: "6690caa54dc3eac2b83517d2",
-    username: "streamer_charlie",
-    name: "Charlie Clark",
+    id: "6690caa54dc3eac2b83517ce",
+    username: "techie_alice",
+    name: "Alice Jones",
   },
   {
     id: "6690caa54dc3eac2b83517d0",
     username: "music_bob",
     name: "Bob Brown",
+  },
+  {
+    id: "6690caa54dc3eac2b83517d2",
+    username: "streamer_charlie",
+    name: "Charlie Clark",
   },
   {
     id: "6690caa54dc3eac2b83517d8",
@@ -36,11 +41,6 @@ const contactDetails = [
     id: "6690caa54dc3eac2b83517cc",
     username: "bookworm_jane",
     name: "Jane Smith",
-  },
-  {
-    id: "6690caa54dc3eac2b83517ce",
-    username: "techie_alice",
-    name: "Alice Jones",
   },
 ];
 
@@ -97,9 +97,12 @@ const addContacts = async (credentials, contactDetails) => {
     {
       query: `mutation AddContacts($userIds: [ID!]) {
         addContacts(userIds: $userIds) {
+          id
+          username
           contacts {
             id
             username
+            name
           }
         }
     }`,
@@ -109,6 +112,8 @@ const addContacts = async (credentials, contactDetails) => {
     },
     credentials.token
   );
+
+  console.log(response.body.data.addContacts);
 
   return response;
 };
@@ -225,6 +230,72 @@ describe("Server tests", () => {
       expect(response.body.data.me.username).toBe(credentials.username);
     });
 
+    it("Should return valid contacts when querying users contacts", async () => {
+      await createUser(credentials);
+      await loginUser(credentials);
+
+      await addContacts(credentials, contactDetails.slice(0, 3));
+
+      const response = await requestData(
+        {
+          query: `query AllContactsByUser($searchByName: String) {
+            allContactsByUser(searchByName: $searchByName) {
+              id
+              username
+              contacts {
+                id
+                username
+                name
+              }
+            }
+          }`,
+          variables: { searchByName: "" },
+        },
+        credentials.token
+      );
+
+      expect(JSON.parse(response.text).errors).toBeUndefined();
+
+      const user = response.body.data.allContactsByUser;
+      console.log("user", user);
+      expect(user.contacts.length).toBe(3);
+
+      expect(user.contacts[0].username).toBe(contactDetails[0].username);
+      expect(user.contacts[1].username).toBe(contactDetails[1].username);
+      expect(user.contacts[2].username).toBe(contactDetails[2].username);
+    });
+
+    it("Should return contacts that are not in the user's contacts", async () => {
+      await createUser(credentials);
+      await loginUser(credentials);
+
+      await addContacts(credentials, [contactDetails[0], contactDetails[1]]);
+
+      const response = await requestData(
+        {
+          query: `query AllContactsExceptByUser($searchByName: String) {
+            allContactsExceptByUser(searchByName: $searchByName) {
+              id
+              username
+              name
+            }
+          }`,
+          variables: { searchByName: "" },
+        },
+        credentials.token
+      );
+
+      expect(JSON.parse(response.text).errors).toBeUndefined();
+
+      const contacts = response.body.data.allContactsExceptByUser;
+
+      contacts.forEach((contact) => {
+        expect([contactDetails[0].id, contactDetails[1].id]).not.toContain(
+          contact.id
+        );
+      });
+    });
+
     it("Add three new contacts", async () => {
       await createUser(credentials);
       await loginUser(credentials);
@@ -236,13 +307,13 @@ describe("Server tests", () => {
       expect(JSON.parse(response.text).errors).toBeUndefined();
       assert.strictEqual(response.body.data.addContacts.contacts.length, 3);
       expect(response.body.data.addContacts.contacts[0].username).toBe(
-        "streamer_charlie"
+        "techie_alice"
       );
       expect(response.body.data.addContacts.contacts[1].username).toBe(
         "music_bob"
       );
       expect(response.body.data.addContacts.contacts[2].username).toBe(
-        "history_frank"
+        "streamer_charlie"
       );
     });
 
@@ -399,8 +470,7 @@ describe("Server tests", () => {
         credentials,
         [
           credentials.id,
-          "6690caa54dc3eac2b83517d0",
-          "6690caa54dc3eac2b83517d8",
+          ...contactDetails.slice(0, 2).map((contact) => contact.id),
         ],
         groupChatDetails[0].title,
         groupChatDetails[0].description
@@ -424,10 +494,10 @@ describe("Server tests", () => {
         credentials.username
       );
       expect(response.body.data.createChat.members[1].username).toBe(
-        contactDetails[1].username
+        contactDetails[0].username
       );
       expect(response.body.data.createChat.members[2].username).toBe(
-        contactDetails[2].username
+        contactDetails[1].username
       );
     });
 
