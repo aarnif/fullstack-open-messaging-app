65552e411dbf5cb9e2ef7e881b784cb9f70427fd
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-04-04 18:09:07 +0300

    Refactor and add tests to ContactCard-component

--- frontend/src/components/Contacts/ContactCard.jsx
+++ frontend/src/components/Contacts/ContactCard.jsx
@@ -1,6 +1,9 @@
 const ContactCard = ({ user, item, admin }) => {
+  const isCurrentUser = user?.id === item.id;
+  const isAdmin = admin?.id === item.id;
+
   return (
-    <div className="mr-4 flex-grow flex items-start">
+    <div data-testid="contact-card" className="mr-4 flex-grow flex items-start">
       <div>
         <img
           className="w-14 h-14 sm:w-16 sm:h-16 rounded-full"
@@ -9,45 +12,22 @@ const ContactCard = ({ user, item, admin }) => {
         />
       </div>
       <div className="flex-grow ml-4">
-        {user?.id === item.id ? (
-          <>
-            <div className="flex justify-between items-start">
-              <div className="text-mobile lg:text-base text-slate-800 dark:text-slate-100 font-bold">
-                You{" "}
-                {admin?.id === item.id && (
-                  <span className="text-mobile lg:text-base text-slate-700 dark:text-slate-200 font-semibold">
-                    Admin
-                  </span>
-                )}
-              </div>
-              <div className="text-mobile lg:text-base text-slate-600 dark:text-slate-200 font-bold">
-                @{item.username}
-              </div>
-            </div>
-            <div className="text-sm text-slate-600 dark:text-slate-200 text-left">
-              {item.about}
-            </div>
-          </>
-        ) : (
-          <>
-            <div className="flex justify-between items-start">
-              <div className="text-mobile lg:text-base text-slate-800 dark:text-slate-100 font-bold">
-                {item.name}{" "}
-                {admin?.id === item.id && (
-                  <span className="text-mobile lg:text-base text-slate-700 dark:text-slate-200 font-semibold">
-                    Admin
-                  </span>
-                )}
-              </div>
-              <div className="text-mobile lg:text-base text-slate-600 dark:text-slate-200 font-bold">
-                @{item.username}
-              </div>
-            </div>
-            <div className="text-sm text-slate-600 dark:text-slate-200 text-left">
-              {item.about}
-            </div>
-          </>
-        )}
+        <div className="flex justify-between items-start">
+          <div className="text-mobile lg:text-base text-slate-800 dark:text-slate-100 font-bold">
+            {isCurrentUser ? "You" : item.name}{" "}
+            {isAdmin && (
+              <span className="text-mobile lg:text-base text-slate-700 dark:text-slate-200 font-semibold">
+                Admin
+              </span>
+            )}
+          </div>
+          <div className="text-mobile lg:text-base text-slate-600 dark:text-slate-200 font-bold">
+            @{item.username}
+          </div>
+        </div>
+        <div className="text-sm text-slate-600 dark:text-slate-200 text-left">
+          {item.about}
+        </div>
       </div>
     </div>
   );
