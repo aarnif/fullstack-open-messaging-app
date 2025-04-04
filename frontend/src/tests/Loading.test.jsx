0d8e153850572e240c694431f7faf719b6f0834e
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-04-04 22:04:41 +0300

    Add test to Loading-component

---
+++ frontend/src/tests/Loading.test.jsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react";
+import { describe } from "vitest";
+
+import Loading from "../components/Loading.jsx";
+
+describe("<Loading />", () => {
+  test("renders component", () => {
+    render(<Loading maxHeight={null} loadingText={"Loading..."} />);
+    expect(screen.getByTestId("loading")).toBeInTheDocument();
+    expect(screen.getByTestId("loading-text")).toBeInTheDocument();
+    expect(screen.getByTestId("loading-text").textContent).toBe("Loading...");
+  });
+});
