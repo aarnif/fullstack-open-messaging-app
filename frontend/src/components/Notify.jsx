f5f480b0250cf6d8879932507a1a237e600f6286
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-04-04 12:42:45 +0300

    Add tests to Notify-component

--- frontend/src/components/Notify.jsx
+++ frontend/src/components/Notify.jsx
@@ -3,7 +3,10 @@ const Notify = ({ notifyMessage }) => {
     return null;
   }
   return (
-    <div className="p-2 flex justify-center items-center bg-red-400 rounded-lg">
+    <div
+      data-testid="notify-message"
+      className="p-2 flex justify-center items-center bg-red-400 rounded-lg"
+    >
       <span className="text-md text-red-700">{notifyMessage.content}</span>
     </div>
   );
