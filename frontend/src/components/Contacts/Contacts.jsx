966061c5c3488988323b2fdce3cb9bb59a449b47
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-04-06 19:53:41 +0300

    Add test to Contacts-component

--- frontend/src/components/Contacts/Contacts.jsx
+++ frontend/src/components/Contacts/Contacts.jsx
@@ -1,6 +1,6 @@
 const Contacts = ({ menuComponent }) => {
   return (
-    <div className="flex-grow flex">
+    <div data-testid="contacts-page" className="flex-grow flex">
       {menuComponent}
       <div className="hidden flex-grow lg:flex justify-center items-center">
         <div className="p-2 rounded-lg bg-slate-200 dark:bg-slate-700 text-slate-800 dark:text-slate-100 font-bold">
---
+++ frontend/src/tests/Contacts.test.jsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import { describe, test, expect } from "vitest";
+
+import Contacts from "../components/Contacts/Contacts.jsx";
+
+describe("<Contacts />", () => {
+  test("renders contacts page", () => {
+    render(<Contacts menuComponent={<></>} />);
+
+    expect(screen.getByTestId("contacts-page")).toBeInTheDocument();
+    expect(screen.getByTestId("contacts-page")).toHaveTextContent(
+      "Select a contact for further information."
+    );
+  });
+});
