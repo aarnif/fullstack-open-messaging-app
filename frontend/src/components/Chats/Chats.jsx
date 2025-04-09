f2b5072b83b76e06eaeae79051ffd45c12db3f2f
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-04-09 19:29:23 +0300

    Add test to Chats-component

--- frontend/src/components/Chats/Chats.jsx
+++ frontend/src/components/Chats/Chats.jsx
@@ -1,6 +1,6 @@
 const Chats = ({ menuComponent }) => {
   return (
-    <div className="flex-grow flex">
+    <div data-testid="chats-page" className="flex-grow flex">
       {menuComponent}
       <div className="hidden flex-grow lg:flex justify-center items-center">
         <div className="p-2 rounded-lg bg-slate-200 dark:bg-slate-700 text-slate-800 dark:text-slate-100 font-bold">
---
+++ frontend/src/tests/Chats.test.jsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import { describe, test, expect } from "vitest";
+
+import Chats from "../components/Chats/Chats.jsx";
+
+describe("<Chats />", () => {
+  test("renders chats page", () => {
+    render(<Chats menuComponent={<></>} />);
+
+    expect(screen.getByTestId("chats-page")).toBeInTheDocument();
+    expect(screen.getByTestId("chats-page")).toHaveTextContent(
+      "Select Chat to Start Messaging."
+    );
+  });
+});
