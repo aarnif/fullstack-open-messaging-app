7be320506f7c409695408c6aec9a54ddf2b9e8bf
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-06-07 22:11:09 +0300

    Add test-id to Input-component

--- frontend/src/components/ui/Input.jsx
+++ frontend/src/components/ui/Input.jsx
@@ -1,8 +1,8 @@
-const Input = ({ item }) => {
+const Input = ({ item, testId }) => {
   return (
     <div className="w-full flex-grow flex flex-col p-2 border-2 border-slate-100 dark:border-slate-500 rounded-lg bg-slate-100 dark:bg-slate-500 hover:border-violet-500 focus-within:border-violet-500 transition">
       <input
-        data-testid="username-input"
+        data-testid={testId}
         className="w-full text-slate-800 dark:text-slate-100 placeholder:text-slate-800 dark:placeholder:text-slate-100 bg-slate-100 dark:bg-slate-500 focus:outline-none focus:bg-opacity-0"
         {...item}
       />
