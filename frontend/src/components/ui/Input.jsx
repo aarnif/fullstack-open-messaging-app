39842139a77db45e89f15d33895578389ec5f02f
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-06-07 22:04:44 +0300

    Add Input-component

---
+++ frontend/src/components/ui/Input.jsx
@@ -0,0 +1,13 @@
+const Input = ({ item }) => {
+  return (
+    <div className="w-full flex-grow flex flex-col p-2 border-2 border-slate-100 dark:border-slate-500 rounded-lg bg-slate-100 dark:bg-slate-500 hover:border-violet-500 focus-within:border-violet-500 transition">
+      <input
+        data-testid="username-input"
+        className="w-full text-slate-800 dark:text-slate-100 placeholder:text-slate-800 dark:placeholder:text-slate-100 bg-slate-100 dark:bg-slate-500 focus:outline-none focus:bg-opacity-0"
+        {...item}
+      />
+    </div>
+  );
+};
+
+export default Input;
