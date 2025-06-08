2c4ff1414dd6bb907774ac15c895b762c61e367c
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-06-08 10:53:23 +0300

    Update sign up component styles

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -4,7 +4,12 @@ import { useMutation, useApolloClient } from "@apollo/client";
 
 import { CREATE_USER, LOGIN } from "../graphql/mutations";
 import useNotifyMessage from "../hooks/useNotifyMessage";
+
+import Title from "./ui/Title";
 import Notify from "./Notify";
+import Label from "./ui/Label";
+import Input from "./ui/Input";
+import Button from "./ui/Button";
 
 const SignUp = ({ setActiveMenuItem }) => {
   const client = useApolloClient();
@@ -61,114 +66,52 @@ const SignUp = ({ setActiveMenuItem }) => {
   };
 
   return (
-    <div className="flex-grow w-full flex justify-center items-center">
+    <div
+      data-testid="sign-up-page"
+      className="flex-grow w-full flex justify-center items-center"
+    >
       <div
-        className="flex-grow max-w-[500px] max-h-[600px] p-8 flex justify-center items-center
-         lg:bg-white dark:bg-slate-800 bg-opacity-90 lg:rounded-xl text-black lg:shadow-xl"
+        className="mx-4 w-full max-w-[450px] p-6 sm:p-8 flex justify-center items-center
+         bg-white dark:bg-slate-800 rounded-xl text-black shadow-xl"
       >
-        <form
-          onSubmit={handleSubmit}
-          className="flex-grow h-full flex flex-col"
-        >
-          <h1
-            data-testid="sign-up-title"
-            className="text-xl lg:text-2xl font-bold text-slate-800 dark:text-slate-100"
-          >
-            Sign Up
-          </h1>
-          <ul>
-            <li className="my-4 w-full flex-grow flex flex-col">
-              <Notify notifyMessage={showNotifyMessage} />
-            </li>
+        <form onSubmit={handleSubmit} className="w-full flex flex-col gap-6">
+          <Title variant="primary" testId="sign-up-title" text="Sign Up" />
 
-            <li className="w-full flex-grow flex flex-col">
-              <label className="text-mobile sm:text-base font-medium text-slate-700 dark:text-slate-200">
-                USERNAME:
-              </label>
-            </li>
-            <li className="w-full flex-grow flex flex-col p-2 border-2 border-slate-100 dark:border-slate-500 rounded-lg bg-slate-100 dark:bg-slate-500 hover:border-violet-500 focus-within:border-violet-500 transition">
-              <input
-                data-testid="username-input"
-                className="w-full text-slate-800 dark:text-slate-100 placeholder:text-slate-800 dark:placeholder:text-slate-100 bg-slate-100 dark:bg-slate-500 focus:outline-none focus:bg-opacity-0"
-                {...username}
-              />
-            </li>
-            <li>
-              {username.value.length === 0 && (
-                <span className="pl-[10px] text-mobile sm:text-base text-red-500">
-                  Please enter your username
-                </span>
-              )}
-            </li>
+          <div className="w-full flex flex-col gap-3">
+            <Notify notifyMessage={showNotifyMessage} />
 
-            <li className="w-full flex-grow flex flex-col">
-              <label className="text-mobile sm:text-base font-medium text-slate-700 dark:text-slate-200">
-                PASSWORD:
-              </label>
-            </li>
+            <div className="w-full flex flex-col">
+              <Label title="username" />
+              <Input item={username} testId="username-input" />
+            </div>
 
-            <li className="w-full flex-grow flex flex-col p-2 border-2 border-slate-100 dark:border-slate-500 rounded-lg bg-slate-100 dark:bg-slate-500 hover:border-violet-500 focus-within:border-violet-500 transition">
-              <input
-                data-testid="password-input"
-                className="w-full text-slate-800 dark:text-slate-100 placeholder:text-slate-800 dark:placeholder:text-slate-100 bg-slate-100 dark:bg-slate-500 focus:outline-none focus:bg-opacity-0"
-                {...password}
-              />
-            </li>
-            <li>
-              {password.value.length === 0 && (
-                <span className="pl-[10px] text-mobile sm:text-base text-red-500">
-                  Please enter your password
-                </span>
-              )}
-            </li>
+            <div className="w-full flex flex-col">
+              <Label title="password" />
+              <Input item={password} testId="password-input" />
+            </div>
 
-            <li className="w-full flex-grow flex flex-col">
-              <label className="text-mobile sm:text-base font-medium text-slate-700 dark:text-slate-200">
-                CONFIRM PASSWORD:
-              </label>
-            </li>
+            <div className="w-full flex flex-col">
+              <Label title="confirm password" />
+              <Input item={confirmPassword} testId="confirm-password-input" />
+            </div>
+          </div>
 
-            <li className="w-full flex-grow flex flex-col p-2 border-2 border-slate-100 dark:border-slate-500 rounded-lg bg-slate-100 dark:bg-slate-500 hover:border-violet-500 focus-within:border-violet-500 transition">
-              <input
-                data-testid="confirm-password-input"
-                className="w-full text-slate-800 dark:text-slate-100 placeholder:text-slate-800 dark:placeholder:text-slate-100 bg-slate-100 dark:bg-slate-500 focus:outline-none focus:bg-opacity-0"
-                {...confirmPassword}
-              />
-            </li>
-            <li>
-              {password.value.length === 0 && (
-                <span className="pl-[10px] text-mobile sm:text-base text-red-500">
-                  Please confirm your password
-                </span>
-              )}
-            </li>
+          <div className="w-full flex flex-col gap-3">
+            <Button
+              type="submit"
+              variant="primary"
+              testId="sign-up-submit-button"
+              text="Sign Up"
+            />
 
-            <li className="my-4 w-full flex-grow flex flex-col">
-              <button
-                type="submit"
-                data-testid="sign-up-submit-button"
-                className="flex-grow py-4 sm:py-6 bg-green-500 border-2 border-green-500 rounded-xl
-                hover:bg-green-600 focus:bg-green-600 active:scale-95 transition"
-              >
-                <div className="text-lg sm:text-xl font-bold text-white">
-                  Sign Up
-                </div>
-              </button>
-            </li>
-            <li className="my-4 w-full flex-grow flex flex-col ">
-              <button
-                onClick={handleClickReturn}
-                type="button"
-                data-testid="return-to-sign-in-button"
-                className="w-full flex-grow py-4 sm:py-6 border-2 border-slate-400 rounded-xl
-hover:bg-slate-300 dark:hover:bg-slate-900 focus:bg-slate-300 active:scale-95 transition"
-              >
-                <div className="text-lg sm:text-xl font-bold text-slate-700 dark:text-slate-300">
-                  Return to Sign In
-                </div>
-              </button>
-            </li>
-          </ul>
+            <Button
+              type="button"
+              variant="secondary"
+              testId="return-to-sign-in-button"
+              text="Return to Sign In"
+              onClick={handleClickReturn}
+            />
+          </div>
         </form>
       </div>
     </div>
