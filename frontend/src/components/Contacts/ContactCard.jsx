6e9bfa1da4b115b03fc5ddf297338e8e8dbffeb8
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-06-08 22:34:10 +0300

    Update contact card styles

--- frontend/src/components/Contacts/ContactCard.jsx
+++ frontend/src/components/Contacts/ContactCard.jsx
@@ -1,33 +1,40 @@
+import Title from "../ui/Title";
+
 const ContactCard = ({ user, item, admin }) => {
   const isCurrentUser = user?.id === item.id;
   const isAdmin = admin?.id === item.id;
 
   return (
-    <div data-testid="contact-card" className="mr-4 flex-grow flex items-start">
-      <div>
-        <img
-          className="w-14 h-14 sm:w-16 sm:h-16 rounded-full"
-          src={item.image.thumbnail}
-          alt={item.name}
-        />
-      </div>
-      <div className="flex-grow ml-4">
+    <div data-testid="contact-card" className="w-full flex items-start gap-4">
+      <img
+        className="w-14 h-14 sm:w-16 sm:h-16 rounded-full"
+        src={item.image.thumbnail}
+        alt={item.name}
+      />
+
+      <div className="w-full flex flex-col">
         <div className="flex justify-between items-start">
-          <div className="text-mobile lg:text-base text-slate-800 dark:text-slate-100 font-bold">
-            {isCurrentUser ? "You" : item.name}{" "}
-            {isAdmin && (
-              <span className="text-mobile lg:text-base text-slate-700 dark:text-slate-200 font-semibold">
-                Admin
-              </span>
-            )}
-          </div>
-          <div className="text-mobile lg:text-base text-slate-600 dark:text-slate-200 font-bold">
+          <Title
+            variant="tertiary"
+            testId={`contact-title-${item.id}`}
+            text={
+              <>
+                {isCurrentUser ? "You" : item.name}{" "}
+                {isAdmin && (
+                  <span className="text-mobile lg:text-base text-slate-600 dark:text-slate-300 font-semibold">
+                    Admin
+                  </span>
+                )}
+              </>
+            }
+          />
+          <p className="text-mobile lg:text-base text-slate-600 dark:text-slate-200 font-bold">
             @{item.username}
-          </div>
+          </p>
         </div>
-        <div className="text-sm text-slate-600 dark:text-slate-200 text-left">
+        <p className="text-mobile lg:text-base text-slate-600 dark:text-slate-200 text-left">
           {item.about}
-        </div>
+        </p>
       </div>
     </div>
   );
