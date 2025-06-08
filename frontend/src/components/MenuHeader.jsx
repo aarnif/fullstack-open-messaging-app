e5cfc49d88efa7ec53849d12fc6058bfcec396db
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-06-08 12:40:04 +0300

    Update menu header component styles

--- frontend/src/components/MenuHeader.jsx
+++ frontend/src/components/MenuHeader.jsx
@@ -1,30 +1,30 @@
-import SearchBar from "./SearchBar";
+import Title from "./ui/Title";
+import Input from "./ui/Input";
+import { FaSearch } from "react-icons/fa";
 import { MdOpenInNew } from "react-icons/md";
 
 const MenuHeader = ({ title, handleCallBack, searchWord }) => {
   return (
     <div data-testid="menu-header" className="p-4">
-      <div className="flex justify-between items-center">
-        <h1
-          data-testid="menu-header-title"
-          className="text-xl lg:text-2xl text-slate-800 dark:text-slate-100 font-bold"
-        >
-          {title}
-        </h1>
+      <div className="mb-2 flex justify-between items-center">
+        <Title variant="primary" testId="menu-header-title" text={title} />
         <button
           onClick={handleCallBack}
           data-testid={
             title === "Chats" ? "new-chat-button" : "new-contact-button"
           }
         >
-          <MdOpenInNew className="w-6 h-6 lg:w-7 lg:h-7 text-slate-700 dark:text-slate-100 fill-current" />
+          <MdOpenInNew className="w-6 h-6 lg:w-7 lg:h-7 text-slate-700 dark:text-slate-100 hover:text-slate-900 dark:hover:text-slate-300 fill-current" />
         </button>
       </div>
 
-      <SearchBar
-        searchWord={searchWord}
-        dataTestId={
+      <Input
+        item={searchWord}
+        testId={
           title === "Contacts" ? "contact-search-input" : "chat-search-input"
+        }
+        icon={
+          <FaSearch className="w-4 h-4 text-slate-800 dark:text-slate-100 fill-current" />
         }
       />
     </div>
--- frontend/src/components/ui/Input.jsx
+++ frontend/src/components/ui/Input.jsx
@@ -1,9 +1,10 @@
-const Input = ({ item, testId }) => {
+const Input = ({ item, testId, icon = <></> }) => {
   return (
-    <div className="w-full flex-grow flex flex-col p-2 border-2 border-slate-100 dark:border-slate-500 rounded-lg bg-slate-100 dark:bg-slate-500 hover:border-violet-500 focus-within:border-violet-500 transition">
+    <div className="w-full flex justify-between items-center gap-2 p-2 border-2 border-slate-100 dark:border-slate-500 rounded-lg bg-slate-100 dark:bg-slate-500 hover:border-violet-500 focus-within:border-violet-500 transition">
+      {icon}
       <input
         data-testid={testId}
-        className="w-full text-slate-800 dark:text-slate-100 placeholder:text-slate-800 dark:placeholder:text-slate-100 bg-slate-100 dark:bg-slate-500 focus:outline-none focus:bg-opacity-0"
+        className="w-full text-mobile lg:text-base text-slate-800 dark:text-slate-100 placeholder:text-slate-800 dark:placeholder:text-slate-100 bg-slate-100 dark:bg-slate-500 focus:outline-none focus:bg-opacity-0"
         {...item}
       />
     </div>
