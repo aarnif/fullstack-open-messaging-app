9d8ba5203e2c34074161bfe33948f3aafd838bae
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-06-08 12:05:27 +0300

    Update Menu component styles

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -11,6 +11,15 @@ export default {
         light: "url('/images/background-light.svg')",
         dark: "url('/images/background-dark.svg')",
       },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0, transform: "translateX(-20px)" },
+          "100%": { opacity: 1, transform: "translateX(0px)" },
+        },
+      },
+      animation: {
+        "fade-in": "fadeIn 0.2s ease-in-out",
+      },
     },
   },
   plugins: [],
