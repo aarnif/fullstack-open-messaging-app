b08bccc7c07e9c2b75535945a02aa5445ce92905
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-06-11 16:02:59 +0300

    Update NewPrivateChatModal.jsx styles

--- frontend/src/components/Modals/NewPrivateChatModal/NewPrivateChatModal.jsx
+++ frontend/src/components/Modals/NewPrivateChatModal/NewPrivateChatModal.jsx
@@ -2,8 +2,7 @@ import { useState } from "react";
 import { motion } from "framer-motion";
 import { useQuery, useLazyQuery } from "@apollo/client";
 import { useNavigate } from "react-router";
-import { MdClose } from "react-icons/md";
-import { IoChevronForward } from "react-icons/io5";
+import { FaSearch } from "react-icons/fa";
 
 import {
   ALL_CONTACTS_BY_USER,
@@ -14,7 +13,9 @@ import useWindowDimensions from "../../../hooks/useWindowDimensions";
 import useField from "../../../hooks/useField";
 
 import Loading from "../../Loading";
-import SearchBar from "../../SearchBar";
+import Input from "../../ui/Input";
+import Button from "../../ui/Button";
+import Title from "../../ui/Title";
 import SelectContactList from "./SelectContactList";
 
 import useNotifyMessage from "../../../hooks/useNotifyMessage";
@@ -87,7 +88,7 @@ const NewPrivateChatModal = ({ user, setShowNewPrivateChatModal }) => {
   return (
     <motion.div
       key={"Overlay"}
-      className="fixed inset-0 flex justify-center items-end sm:items-center bg-black bg-opacity-50 z-10 transition"
+      className="fixed inset-0 flex justify-center items-end sm:items-center bg-black/50 z-10 transition"
       onClick={() => setShowNewPrivateChatModal(false)}
       initial={{ width: "0vw", opacity: 0 }}
       animate={{ width: "100vw", opacity: 1, duration: 1.0 }}
@@ -96,52 +97,56 @@ const NewPrivateChatModal = ({ user, setShowNewPrivateChatModal }) => {
       <motion.div
         data-testid="new-private-chat-modal"
         key={"newChatModal"}
-        className="w-full h-[90vh] sm:w-[500px] sm:h-[600px] bg-white dark:bg-slate-800 rounded-xl text-slate-800 dark:text-slate-100 z-100"
+        className="w-full h-[90vh] sm:max-w-[500px] sm:max-h-[600px] bg-white dark:bg-slate-800 rounded-t-xl sm:rounded-xl"
         onClick={(e) => e.stopPropagation()}
         initial={{ y: width <= 640 ? 50 : -50, opacity: 0 }}
         animate={{ y: 0, opacity: 1, duration: 0.4 }}
         exit={{ y: width <= 640 ? 50 : -50, opacity: 0 }}
         transition={{ delay: 0.4, type: "tween" }}
       >
-        <div className="h-full flex-grow flex flex-col py-4 px-4">
-          <div className="w-full flex justify-center items-center pb-2">
-            <button
-              data-testid="close-new-private-chat-modal"
+        <div className="p-4 h-full flex flex-col gap-4">
+          <div className="w-full flex justify-between items-center">
+            <Button
+              type="button"
+              variant="close"
+              testId="close-add-new-contacts-modal-button"
               onClick={() => setShowNewPrivateChatModal(false)}
-            >
-              <MdClose className="w-6 h-6 sm:w-7 sm:h-7 text-slate-800 dark:text-slate-100 fill-current" />
-            </button>
-            <h2 className="flex-grow text-xl sm:text-2xl font-bold text-slate-800 dark:text-slate-100 text-center">
-              New Private Chat
-            </h2>
-            <button
-              onClick={handleCreatePrivateChat}
-              data-testid="start-new-private-chat-button"
-            >
-              <IoChevronForward className="w-6 h-6 sm:w-7 sm:h-7 text-slate-800 dark:text-slate-100 fill-current" />
-            </button>
-          </div>
-          <>
-            <Notify notifyMessage={notifyMessage} />
-            <SearchBar
-              searchWord={searchWord}
-              dataTestId={"search-contacts-input"}
             />
-            {result.loading ? (
-              <Loading />
-            ) : (
-              <>
-                <div className="flex-grow w-full overflow-y-auto h-0">
-                  <SelectContactList
-                    user={user}
-                    data={result.data.allContactsByUser.contacts}
-                    chosenUserId={chosenUserId}
-                    setChosenUserId={setChosenUserId}
-                  />
-                </div>
-              </>
-            )}
-          </>
+
+            <Title
+              variant="secondary"
+              testId="add-new-contacts-modal-title"
+              text="New Private Chat"
+            />
+
+            <Button
+              type="button"
+              variant="forward"
+              testId="start-new-private-chat-button"
+              onClick={handleCreatePrivateChat}
+            />
+          </div>
+
+          <Notify notifyMessage={notifyMessage} />
+          <Input
+            item={searchWord}
+            testId="search-contacts-input"
+            icon={
+              <FaSearch className="w-4 h-4 text-slate-800 dark:text-slate-100 fill-current" />
+            }
+          />
+          {result.loading ? (
+            <Loading />
+          ) : (
+            <div className="flex-grow w-full overflow-y-auto">
+              <SelectContactList
+                user={user}
+                data={result.data.allContactsByUser.contacts}
+                chosenUserId={chosenUserId}
+                setChosenUserId={setChosenUserId}
+              />
+            </div>
+          )}
         </div>
       </motion.div>
     </motion.div>
