443e764d075b99b29585973739ac70192cd23ed4
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-06-11 22:52:23 +0300

    Tweak Profile page styles

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -5,6 +5,7 @@ import IndividualContactCard from "./ui/IndividualContactCard";
 import EditProfileModal from "./Modals/EditProfileModal";
 import Button from "./ui/Button";
 
+// Add go back button to mobile view
 const Profile = ({ user, menuComponent }) => {
   const [showEditProfileModal, setShowEditProfileModal] = useState(false);
 
@@ -16,7 +17,7 @@ const Profile = ({ user, menuComponent }) => {
       <div className="hidden flex-grow lg:max-w-[450px] lg:flex">
         {menuComponent}
       </div>
-      <div className="p-4 relative flex-grow flex flex-col justify-center items-start">
+      <div className="p-4 relative flex-grow flex flex-col justify-start items-center gap-4">
         <div className="w-full flex justify-end">
           <Button
             type="button"
