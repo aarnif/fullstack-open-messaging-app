84c703a12efe02b5a15f4801ea6360b0bf66df29
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-06-14 12:40:40 +0300

    Remove ChatsMenu-, ContactsMenu- and MenuHeader-components

--- frontend/src/tests/MenuHeader.test.jsx
+++ frontend/src/tests/MenuHeader.test.jsx
@@ -3,7 +3,7 @@ import { describe, test, expect } from "vitest";
 import userEvent from "@testing-library/user-event";
 import queryMocks from "./mocks/queryMocks.js";
 
-import MenuHeader from "../components/ui/MenuHeader";
+import { MenuHeader } from "../components/ListMenu.jsx";
 
 const { mockSearchWord } = queryMocks;
 
