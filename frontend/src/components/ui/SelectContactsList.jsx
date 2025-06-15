07283eb46220ded233d2cb4433590215610ef3f6
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-06-15 18:23:50 +0300

    Move SelectContactsItem-component inside SelectContactsList.jsx

--- frontend/src/components/ui/SelectContactsList.jsx
+++ frontend/src/components/ui/SelectContactsList.jsx
@@ -1,5 +1,44 @@
+import { useState, useEffect } from "react";
+import { MdCheck } from "react-icons/md";
+
 import Title from "./Title";
-import SelectContactsItem from "../Modals/SelectContactsItem";
+import ContactCard from "./ContactCard";
+
+const SelectContactsItem = ({ item, chosenUserIds, setChosenUserIds }) => {
+  const [isChosen, setIsChosen] = useState(false);
+
+  useEffect(() => {
+    const checkIfUserIsChosen = chosenUserIds.includes(item.id);
+    if (checkIfUserIsChosen) setIsChosen(true);
+  }, [chosenUserIds, item.id]);
+
+  const handlePress = () => {
+    if (isChosen) {
+      setChosenUserIds((prev) => [...prev.filter((id) => id !== item.id)]);
+    } else {
+      setChosenUserIds((prev) => [...prev, item.id]);
+    }
+    setIsChosen(!isChosen);
+  };
+
+  return (
+    <div
+      onClick={handlePress}
+      data-testid={`contact-${item.username}`}
+      className="flex items-center px-0 py-2 sm:px-4 rounded-lg cursor-pointer"
+    >
+      <ContactCard item={item} />
+
+      {isChosen ? (
+        <div className="w-6 h-6 flex justify-center items-center border border-green-600 bg-green-600 rounded-full">
+          <MdCheck size={20} className="text-white" />
+        </div>
+      ) : (
+        <div className="w-6 h-6 flex justify-center items-center border border-slate-300 rounded-full"></div>
+      )}
+    </div>
+  );
+};
 
 const SelectContactsList = ({ data, chosenUserIds, setChosenUserIds }) => {
   if (!data.length) {
