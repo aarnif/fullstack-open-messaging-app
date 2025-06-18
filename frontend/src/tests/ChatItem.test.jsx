ebf2aa1980adbfe56a984a7c782a3b2540a14006
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-06-18 17:11:42 +0300

    Fix failing ChatItem-tests

--- frontend/src/tests/ChatItem.test.jsx
+++ frontend/src/tests/ChatItem.test.jsx
@@ -8,11 +8,11 @@ import queryMocks from "./mocks/queryMocks.js";
 import mocks from "./mocks/funcs.js";
 import chatAndMessageHelpers from "../helpers/chatAndMessageHelpers.js";
 
-const { currentUserMock, findGroupChatByIdMock } = queryMocks;
+const { currentUserMock, everyChatByUserMock } = queryMocks;
 const { navigate } = mocks;
 
 const userData = currentUserMock.result.data.me;
-const mockChatData = findGroupChatByIdMock.result.data.findChatById;
+const mockChatData = everyChatByUserMock.result.data.everyChatByUser[0];
 
 const mockSetActiveChatOrContactId = vi.fn();
 
@@ -26,15 +26,15 @@ vi.mock("react-router", async () => {
 
 const renderChatItem = (
   chatData = mockChatData,
-  activeChatOrContactId = chatData.id
+  activeChatOrContactId = chatData.chat.id
 ) => {
   return render(
-    <MockedProvider mocks={[currentUserMock, findGroupChatByIdMock]}>
+    <MockedProvider mocks={[currentUserMock, everyChatByUserMock]}>
       <MemoryRouter>
         <ChatItem
           index={0}
           user={userData}
-          chat={chatData}
+          userChat={chatData}
           activeChatOrContactId={activeChatOrContactId}
           setActiveChatOrContactId={mockSetActiveChatOrContactId}
         />
@@ -49,172 +49,183 @@ describe("<ChatItem />", () => {
     useNavigate.mockReturnValue(navigate);
   });
 
-  test("skip tests", () => {
-    expect(true).toBe(true);
+  test("renders chat item with correct info", () => {
+    renderChatItem();
+
+    expect(
+      screen.getByTestId(`chat-item-${mockChatData.chat.id}`)
+    ).toBeInTheDocument();
+    expect(screen.getByTestId("chat-card")).toBeInTheDocument();
+    expect(screen.getByText(mockChatData.chat.title)).toBeInTheDocument();
+    expect(screen.queryByText("No messages")).not.toBeInTheDocument();
   });
 
-  // test("renders chat item with correct info", () => {
-  //   renderChatItem();
+  test("renders nothing when chat has no messages", () => {
+    const chatWithNoMessages = {
+      ...mockChatData,
+      chat: {
+        ...mockChatData.chat,
+        messages: [],
+      },
+    };
 
-  //   expect(
-  //     screen.getByTestId(`chat-item-${mockChatData.id}`)
-  //   ).toBeInTheDocument();
-  //   expect(screen.getByTestId("chat-card")).toBeInTheDocument();
-  //   expect(screen.getByText(mockChatData.title)).toBeInTheDocument();
-  //   expect(screen.queryByText("No messages")).not.toBeInTheDocument();
-  // });
+    const { container } = renderChatItem(chatWithNoMessages);
+    expect(container.firstChild).toBeNull();
+  });
 
-  // test("renders nothing when chat has no messages", () => {
-  //   const chatWithNoMessages = {
-  //     ...mockChatData,
-  //     messages: [],
-  //   };
+  test("highlights active contact", () => {
+    renderChatItem();
 
-  //   const { container } = renderChatItem(chatWithNoMessages);
-  //   expect(container.firstChild).toBeNull();
-  // });
+    expect(screen.getByRole("button")).toHaveAttribute("id", "active-chat");
+  });
 
-  // test("highlights active contact", () => {
-  //   renderChatItem();
+  test("does not highlight inactive contact", () => {
+    renderChatItem(mockChatData, "inactive-contact-id");
 
-  //   expect(screen.getByRole("button")).toHaveAttribute("id", "active-chat");
-  // });
+    expect(screen.getByRole("button")).not.toHaveAttribute(
+      "id",
+      "chat-contact"
+    );
+  });
 
-  // test("does not highlight inactive contact", () => {
-  //   renderChatItem(mockChatData, "inactive-contact-id");
+  test("navigates to chat page on click", async () => {
+    const user = userEvent.setup();
+    renderChatItem();
 
-  //   expect(screen.getByRole("button")).not.toHaveAttribute(
-  //     "id",
-  //     "chat-contact"
-  //   );
-  // });
+    await user.click(screen.getByTestId(`chat-item-${mockChatData.chat.id}`));
 
-  // test("navigates to chat page on click", async () => {
-  //   const user = userEvent.setup();
-  //   renderChatItem();
+    expect(navigate).toHaveBeenCalledWith(`/chats/${mockChatData.chat.id}`);
+    expect(mockSetActiveChatOrContactId).toHaveBeenCalledWith(
+      mockChatData.chat.id
+    );
+  });
 
-  //   await user.click(screen.getByTestId(`chat-item-${mockChatData.id}`));
+  test("do not display new messages count if count zero", () => {
+    renderChatItem();
+    expect(screen.queryByTestId("new-messages-count")).not.toBeInTheDocument();
+  });
 
-  //   expect(navigate).toHaveBeenCalledWith(`/chats/${mockChatData.id}`);
-  //   expect(mockSetActiveChatOrContactId).toHaveBeenCalledWith(mockChatData.id);
-  // });
+  test("displays new messages count if count greater than zero", () => {
+    const chatWithNewMessages = {
+      ...mockChatData,
+      unreadMessages: 3,
+    };
+    renderChatItem(chatWithNewMessages);
+    expect(screen.queryByTestId("new-messages-count")).toBeInTheDocument();
+  });
 
-  // test("do not display new messages count if count zero", () => {
-  //   renderChatItem();
-  //   expect(screen.queryByTestId("new-messages-count")).not.toBeInTheDocument();
-  // });
+  test("displays notification message correctly", () => {
+    const chatWithNotification = {
+      ...mockChatData,
+      chat: {
+        ...mockChatData.chat,
+        messages: [
+          {
+            ...mockChatData.chat.messages[0],
+            type: "notification",
+            content: "User joined the chat",
+            sender: { id: "someone-else", name: "Someone Else" },
+          },
+        ],
+      },
+    };
 
-  // // test("displays new messages count if count greater than zero", () => {
-  // //   const chatWithNewMessages = {
-  // //     ...mockChatData,
-  // //     messages: [
-  // //       ...mockChatData.messages,
-  // //       {
-  // //         ...mockChatData.messages[0],
-  // //         sender: { id: userData.id },
-  // //         isReadBy: [{ isRead: false, member: { id: userData.id } }],
-  // //       },
-  // //     ],
-  // //   };
-  // //   renderChatItem(chatWithNewMessages);
-  // //   expect(screen.queryByTestId("new-messages-count")).toBeInTheDocument();
-  // // });
+    renderChatItem(chatWithNotification);
+    expect(screen.getByTestId("latest-chat-message")).toHaveTextContent(
+      "User joined the chat"
+    );
+  });
 
-  // test("displays notification message correctly", () => {
-  //   const chatWithNotification = {
-  //     ...mockChatData,
-  //     messages: [
-  //       {
-  //         ...mockChatData.messages[0],
-  //         type: "notification",
-  //         content: "User joined the chat",
-  //         sender: { id: "someone-else", name: "Someone Else" },
-  //       },
-  //     ],
-  //   };
+  test("displays text message from current user correctly", () => {
+    const message = "Hello world";
 
-  //   renderChatItem(chatWithNotification);
-  //   expect(screen.getByTestId("latest-chat-message")).toHaveTextContent(
-  //     "User joined the chat"
-  //   );
-  // });
+    const chatWithMessage = {
+      ...mockChatData,
+      chat: {
+        ...mockChatData.chat,
+        messages: [
+          {
+            ...mockChatData.chat.messages[0],
+            type: "text",
+            content: message,
+            sender: { id: userData.id, name: userData.name },
+          },
+        ],
+      },
+    };
 
-  // test("displays text message from current user correctly", () => {
-  //   const message = "Hello world";
-  //   const chatWithMessage = {
-  //     ...mockChatData,
-  //     messages: [
-  //       {
-  //         ...mockChatData.messages[0],
-  //         type: "text",
-  //         content: message,
-  //         sender: { id: userData.id, name: userData.name },
-  //       },
-  //     ],
-  //   };
+    renderChatItem(chatWithMessage);
+    expect(screen.getByTestId("latest-chat-message")).toHaveTextContent(
+      `You: ${chatAndMessageHelpers.sliceLatestMessage(message)}`
+    );
+  });
 
-  //   renderChatItem(chatWithMessage);
-  //   expect(screen.getByTestId("latest-chat-message")).toHaveTextContent(
-  //     `You: ${chatAndMessageHelpers.sliceLatestMessage(message)}`
-  //   );
-  // });
+  test("displays text message from another user correctly", () => {
+    const message = "Hello there";
+    const senderName = "Jane Smith";
 
-  // test("displays text message from another user correctly", () => {
-  //   const message = "Hello there";
-  //   const senderName = "Jane Smith";
-  //   const chatWithMessage = {
-  //     ...mockChatData,
-  //     messages: [
-  //       {
-  //         ...mockChatData.messages[0],
-  //         type: "text",
-  //         content: message,
-  //         sender: { id: "another-user", name: senderName },
-  //       },
-  //     ],
-  //   };
+    const chatWithMessage = {
+      ...mockChatData,
+      chat: {
+        ...mockChatData.chat,
+        messages: [
+          {
+            ...mockChatData.chat.messages[0],
+            type: "text",
+            content: message,
+            sender: { id: "another-user", name: senderName },
+          },
+        ],
+      },
+    };
 
-  //   renderChatItem(chatWithMessage);
-  //   expect(screen.getByTestId("latest-chat-message")).toHaveTextContent(
-  //     `${senderName}: ${chatAndMessageHelpers.sliceLatestMessage(message)}`
-  //   );
-  // });
+    renderChatItem(chatWithMessage);
+    expect(screen.getByTestId("latest-chat-message")).toHaveTextContent(
+      `${senderName}: ${chatAndMessageHelpers.sliceLatestMessage(message)}`
+    );
+  });
 
-  // test("displays image message from current user correctly", () => {
-  //   const chatWithImage = {
-  //     ...mockChatData,
-  //     messages: [
-  //       {
-  //         ...mockChatData.messages[0],
-  //         type: "singleImage",
-  //         content: "image-url",
-  //         sender: { id: userData.id, name: userData.name },
-  //       },
-  //     ],
-  //   };
+  test("displays image message from current user correctly", () => {
+    const chatWithImage = {
+      ...mockChatData,
+      chat: {
+        ...mockChatData.chat,
+        messages: [
+          {
+            ...mockChatData.chat.messages[0],
+            type: "singleImage",
+            content: "image-url",
+            sender: { id: userData.id, name: userData.name },
+          },
+        ],
+      },
+    };
 
-  //   renderChatItem(chatWithImage);
-  //   expect(screen.getByTestId("latest-chat-message")).toHaveTextContent(
-  //     "You sent an image"
-  //   );
-  // });
+    renderChatItem(chatWithImage);
+    expect(screen.getByTestId("latest-chat-message")).toHaveTextContent(
+      "You sent an image"
+    );
+  });
 
-  // test("displays image message from another user correctly", () => {
-  //   const chatWithImage = {
-  //     ...mockChatData,
-  //     messages: [
-  //       {
-  //         ...mockChatData.messages[0],
-  //         type: "singleImage",
-  //         content: "image-url",
-  //         sender: { id: "other-user-id", name: "John Doe" },
-  //       },
-  //     ],
-  //   };
+  test("displays image message from another user correctly", () => {
+    const chatWithImage = {
+      ...mockChatData,
+      chat: {
+        ...mockChatData.chat,
+        messages: [
+          {
+            ...mockChatData.chat.messages[0],
+            type: "singleImage",
+            content: "image-url",
+            sender: { id: "other-user-id", name: "John Doe" },
+          },
+        ],
+      },
+    };
 
-  //   renderChatItem(chatWithImage);
-  //   expect(screen.getByTestId("latest-chat-message")).toHaveTextContent(
-  //     "John Doe sent an image"
-  //   );
-  // });
+    renderChatItem(chatWithImage);
+    expect(screen.getByTestId("latest-chat-message")).toHaveTextContent(
+      "John Doe sent an image"
+    );
+  });
 });
